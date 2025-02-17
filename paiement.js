8166d644e446f98e9502f3c908e2d6bb29095998
Amine.H <87930239+hasnape@users.noreply.github.com>
2025-02-17 14:30:34 +0100

    paiement.js
    paiement.js

---
+++ paiement.js
@@ -0,0 +1,7 @@
+document.getElementById("paypal-button").addEventListener("click", function() {
+    alert("Redirection vers PayPal...");
+});
+
+document.getElementById("stripe-button").addEventListener("click", function() {
+    alert("Redirection vers Stripe...");
+});
